c24414c22035ea0abfcd6361c08fe2634687f1cd
WalterHumphrey <walter.humphrey17@gmail.com>
2022-10-10 23:02:12 +0200

    try

--- Practica 1 - caesar/utils.py
+++ Practica 1 - caesar/utils.py
@@ -3,12 +3,14 @@
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 sh_alphabet = {}
 
+# New alphabet shifting from key
 def dictionary(key):
     for i in range(0,26):
         letter = alphabet[i]
         sh_alphabet[letter] = alphabet[(i+key)%26]
     return sh_alphabet
 
+# Encrypts with new alphabet
 def caesar_encrypt(text,key):
     cipher = ''
     dictionary(key)
@@ -16,7 +18,7 @@ def caesar_encrypt(text,key):
         if letter in sh_alphabet:
             letter = sh_alphabet[letter]
             cipher = cipher + letter
-        elif letter == ' ':
+        elif letter == ' ' or letter == '\n':
             cipher = cipher + ' '
         else:
             cipher = cipher + ''
